5eaef97f16d4944233a1ec009eaaa5b3d000a382
tesseract314 <tesseract314@users.noreply.github.com>
2019-03-18 16:41:46 -0700

    Added some proof-of-concept untility functions/files

---
+++ lambdata_tesseract/__init__.py
@@ -0,0 +1,10 @@
+"""
+lambdata - a collection of Data Science helper functions.
+"""
+
+import pandas as pd
+import numpy as np
+
+ONES = pd.DataFrame(np.ones(10))
+
+
---
+++ lambdata_tesseract/df_utils.py
@@ -0,0 +1,9 @@
+"""
+Utility function for working with DataFrames.
+
+"""
+
+import pandas as pd
+
+TEST_DF = pd.DataFrame([1,2,3])
+
